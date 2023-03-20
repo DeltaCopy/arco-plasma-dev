87536b265c8ce903dff3c6ed4216119d78375fe9
Fennec <Fennec>
2023-03-20 12:47:28 +0000

    Switched to array to store packages.

--- installation-scripts/post-install.sh
+++ installation-scripts/post-install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-pacman -R kvantum
-yay -S --noconfirm --cleanafter lightly-git
-yay -S --noconfirm --cleanafter chromium-extension-ublock-origin
+arch_packages=("kvantum")
+aur_packages=("lightly-git" "chromium-extension-ublock-origin")
+
+for arch_pkg in "${arch_packages[@]}"; do
+  pacman -R --noconfirm "$arch_pkg"
+done
+
+for aur_pkg in "${aur_packages[@]}"; do
+  yay -S --noconfirm --cleanafter "$aur_pkg"
+done
