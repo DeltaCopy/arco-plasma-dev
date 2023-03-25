dc7b2ec2a32df4621b3d01f5bad796f7e6b59774
Fennec <Fennec>
2023-03-25 11:38:38 +0000

    Cleanup, and updates to scripts.

--- installation-scripts/pacman-install.sh
+++ installation-scripts/pacman-install.sh
@@ -1,11 +1,51 @@
 #!/usr/bin/bash
 # feed into pacman a list of packages, but   ignore commented packages
+# used for installing packages on a vanilla archlinux system
 set -o pipefail
 
+pacman_conf="/etc/pacman.conf"
+arcolinux_mirrorlist="/etc/pacman.d/arcolinux-mirrorlist"
+if test -s "$pacman_conf"; then
+  cat "$pacman_conf" | grep -qw "arcolinux_repo_iso" && cat "$pacman_conf" | grep -qw "arcolinux_repo" && cat "$pacman_conf" | grep -qw "arcolinux_repo_3party" && echo "[INFO] ArcoLinux setup inside $pacman_conf"
+fi
+
+
+# if this is a vanilla arch install setup pacman.conf, and import key
+
+test -s "$arcolinux_mirrorlist" ||
+  cat << EOF > /etc/pacman.d/arcolinux-mirrorlist
+  # Europe Netherlands Amsterdam
+#Server = https://ant.seedhost.eu/arcolinux/$repo/$arch
+
+# Gitlab United States
+Server = https://gitlab.com/arcolinux/$repo/-/raw/main/$arch
+
+# Sweden - accum.se
+Server = https://mirror.accum.se/mirror/arcolinux.info/$repo/$arch
+
+# Europe Belgium Brussels
+Server = https://ftp.belnet.be/arcolinux/$repo/$arch
+
+# Australia
+Server = https://mirror.aarnet.edu.au/pub/arcolinux/$repo/$arch
+
+# South Korea
+Server = https://mirror.funami.tech/arcolinux/$repo/$arch
+
+# Singapore
+Server = https://mirror.jingk.ai/arcolinux/$repo/$arch
+
+# United States San Francisco - no xlarge repo here
+Server = https://arcolinux.github.io/$repo/$arch
+EOF
+
+test -s "$arcolinux_mirrorlist" && echo "[INFO] ArcoLinux mirrorlist setup"
+
+
 packages_x86_64=$1
+if [ -f "$packages_x86_64" ]; then
+ while read pkg; do
 
-test -s "$packages_file" &&
-
-  while read pkg; do
     echo "$pkg" | grep "^[#;]" || sudo pacman -S "$pkg" --needed --noconfirm
-  done < "$packages_x86_64"
+ done < "$packages_x86_64"
+fi
--- installation-scripts/post-install.sh
+++ installation-scripts/post-install.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -o pipefail
-konsave_import_file=$1
+
 arch_packages=("kvantum")
 aur_packages=("lightly-git" "chromium-extension-ublock-origin" "libva-nvidia-driver")
-
-if [ ! -z "$konsave_backup" ]; then
-  if test -s "$konsave_import_file"; then
-    konsave -i "$konsave_import_file"
-  fi
-fi
-
 
 for arch_pkg in "${arch_packages[@]}"; do
   pacman -R --noconfirm "$arch_pkg"
